bcc5cdd3c286797ce3704f8c7c566dbcc8f8d7c2
RawatDevanshu <rawatdevanshu22@gmail.com>
2025-02-18 11:38:11 +0530

    coloured indentation and cursor like chat

--- lua/dev/plugins/indent-blankline.lua
+++ lua/dev/plugins/indent-blankline.lua
@@ -1,39 +1,70 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
-  event = "BufReadPre",
-  opts = function()
-    LazyVim.toggle.map("<leader>ug", {
-      name = "Indention Guides",
-      get = function()
-        return require("ibl.config").get_config(0).enabled
-      end,
-      set = function(state)
-        require("ibl").setup_buffer(0, { enabled = state })
-      end,
-    })
+  tag = "v3.8.2",
+  enabled = true,
+  main = "ibl",
+  opts = {},
+  dependencies = {
+    { "HiPhish/rainbow-delimiters.nvim", lazy = true },
+  },
 
-    return {
+  config = function(_)
+    local highlight = {
+      "RainbowRed",
+      "RainbowYellow",
+      "RainbowBlue",
+      "RainbowOrange",
+      "RainbowGreen",
+      "RainbowViolet",
+      "RainbowCyan",
+      "CursorColumn",
+      "WhiteSpace",
+    }
+    local hooks = require("ibl.hooks")
+    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+      vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+      vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+      vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+      vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+      vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+      vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+      vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+      vim.api.nvim_set_hl(0, "WhiteScope", { fg = "#EEEEEE" })
+    end)
+
+    vim.g.rainbow_delimiters = { highlight = highlight }
+
+    require("ibl").setup({
       indent = {
-        char = "│",
-        tab_char = "│",
+        highlight = highlight,
+        char = "┊",
+        tab_char = "|",
       },
-      scope = { show_start = false, show_end = false },
+      scope = {
+        enabled = true,
+        show_start = false,
+        show_end = false,
+        highlight = { "WhiteScope" },
+      },
+      whitespace = {
+        remove_blankline_trail = false,
+      },
       exclude = {
         filetypes = {
-          "help",
-          "alpha",
-          "dashboard",
-          "neo-tree",
+          "NvimTree",
           "Trouble",
-          "trouble",
-          "lazy",
-          "mason",
+          "dashboard",
+          "git",
+          "help",
+          "markdown",
           "notify",
-          "toggleterm",
-          "lazyterm",
+          "packer",
+          "sagahover",
+          "terminal",
+          "undotree",
         },
+        buftypes = { "terminal", "nofile", "prompt", "quickfix" },
       },
-    }
+    })
   end,
-  main = "ibl",
 }
